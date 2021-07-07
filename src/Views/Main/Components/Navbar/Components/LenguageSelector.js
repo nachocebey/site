e71549b8b823fc413c9b76d64b5a3486b1e3c71f
Nacho Cebey <n.cebey@busup.com>
2021-07-07 18:45:30 +0200

    Trying to style lenguageSelector

--- src/Views/Main/Components/Navbar/Components/LenguageSelector.js
+++ src/Views/Main/Components/Navbar/Components/LenguageSelector.js
@@ -1,40 +1,67 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { useTranslation } from "react-i18next";
 
-const WhiteAlien = '#fcfcfc';
-const SpaceRed = '#d90429';
+const WhiteAlien = "#fcfcfc";
+const SpaceRed = "#d90429";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(4),
     minWidth: 120,
   },
-  select: {
-    color: WhiteAlien,
-    borderColor: WhiteAlien,
-    '&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-      borderColor: WhiteAlien,
-    },
-    '& .MuiOutlinedInput-notchedOutline': {
-      borderColor: "gray",
-    },
-    ':hover &.MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-      borderColor: WhiteAlien,
-    },
-  },
+  // select: {
+  //   color: WhiteAlien,
+  //   borderColor: WhiteAlien,
+  //   "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+  //     borderColor: WhiteAlien,
+  //   },
+  //   "& .MuiOutlinedInput-notchedOutline": {
+  //     borderColor: "gray",
+  //   },
+  //   "& .MuiOutlinedInput-root:hover": {
+  //     borderColor: "blue",
+  //   },
+  // },
 
   inputLabel: {
     color: WhiteAlien,
-    '&.MuiFormLabel-root.Mui-focused': {
+    "&.MuiFormLabel-root.Mui-focused": {
       color: WhiteAlien,
     },
   },
 }));
+
+const StyledSelect = withStyles({
+  root: {
+    color: WhiteAlien,
+    borderColor: WhiteAlien,
+    "&&:hover": {
+      borderColor: "purple",
+    },
+    "&&:focus": {
+      borderColor: WhiteAlien,
+    },
+  },
+})(Select);
+
+
+const StyledInput = withStyles({
+  root: {
+    color: WhiteAlien,
+    borderColor: WhiteAlien,
+    "&&:hover": {
+      borderColor: "purple",
+    },
+    "&&:focus": {
+      borderColor: WhiteAlien,
+    },
+  },
+})(InputLabel);
 
 export default function SimpleSelect() {
   const { t, i18n } = useTranslation();
@@ -51,20 +78,23 @@ export default function SimpleSelect() {
   return (
     <div>
       <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel className={classes.inputLabel} id="lenguage-selector-input-label">
-          {t('Navbar.select_title')}
-        </InputLabel>
-        <Select
+        <StyledInput
+          className={classes.inputLabel}
+          id="lenguage-selector-input-label"
+        >
+          {t("Navbar.select_title")}
+        </StyledInput>
+        <StyledSelect
           className={classes.select}
           labelId="lenguage-selector-label"
           id="lenguage-selector"
           value={i18n.language}
           onChange={handleChange}
-          label={t('Navbar.select_title')}
+          label={t("Navbar.select_title")}
         >
-          <MenuItem value={'es_ES'}>Castellano</MenuItem>
-          <MenuItem value={'en_EN'}>English</MenuItem>
-        </Select>
+          <MenuItem value={"es_ES"}>Castellano</MenuItem>
+          <MenuItem value={"en_EN"}>English</MenuItem>
+        </StyledSelect>
       </FormControl>
     </div>
   );
