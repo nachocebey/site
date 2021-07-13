93b140a40841e03a639f4099f0b2880c981276e8
Nacho Cebey <n.cebey@busup.com>
2021-07-13 20:44:22 +0200

    Creating base structure

--- src/Views/Main/Components/SimpleDescription/SimpleDescription.js
+++ src/Views/Main/Components/SimpleDescription/SimpleDescription.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  descriptionContainer: {
+    justifyContent: "center",
+    paddingTop: "8vh",
+    paddingBottom: "8vh",
+  },
+  descriptionBox: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+  },
+}));
 
 function SimpleDescription() {
   const { t, i18n } = useTranslation();
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
+  const classes = useStyles();
 
   return (
-    <div>
-
-    </div>
+    <Grid
+      container
+      spacing={5}
+      alignItems="center"
+      className={classes.descriptionContainer}
+    >
+      <Grid item xs={6} className={classes.descriptionBox}>
+        <Typography variant="h5" gutterBottom>
+          Nacho Cebey Honorato
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/Views/Root/Root.js
+++ src/Views/Root/Root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Main } from '../Main/main.js';
+import Main  from '../Main/main.js';
 
 import './Root.css';
 
